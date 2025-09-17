bd5cecc0a7041f50a6d3dc5400709b4a8eaf0bbe
Fernanda Locateli <locatelifernanda32gmail.com>
2025-09-17 12:09:17 -0300

    oii

--- Atividade.01/Atividade.01/Program.cs
+++ Atividade.01/Atividade.01/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("CALCULADORA DE HIPOTENUSAS");
+// Adquirindo os dados
 Console.WriteLine("Digite o cateto oposto:");
 double oposto = double.Parse(Console.ReadLine());
 Console.WriteLine("Digite o cateto adjacente:");
 double adjacente = double.Parse(Console.ReadLine());
+// Calculando a hipotenusa
 double resultado = (oposto*oposto) + (adjacente*adjacente);
 double hipotenusa = Math.Sqrt(resultado);
     Console.WriteLine($"A hipotenusa é de aprox: {hipotenusa:F1}");
