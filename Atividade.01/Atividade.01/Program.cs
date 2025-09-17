34960c97e3761122b4df6fb3658f0fd3f98c456f
Fernanda Locateli <locatelifernanda32gmail.com>
2025-09-17 12:02:30 -0300

    Atividade concluida

---
+++ Atividade.01/Atividade.01/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite o cateto oposto:");
+double oposto = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o cateto adjacente:");
+double adjacente = double.Parse(Console.ReadLine());
+double resultado = (oposto*oposto) + (adjacente*adjacente);
+double hipotenusa = Math.Sqrt(resultado);
+    Console.WriteLine($"A hipotenusa é de aprox: {hipotenusa:F1}");
