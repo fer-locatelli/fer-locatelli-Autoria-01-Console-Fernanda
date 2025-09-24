e59d78a0d797d070ea4f22c1fc125a436840cc08
Fernanda Locatelli <locatelifernanda3@gmail.com>
2025-09-24 08:32:19 -0300

    ATIVIDADE02

---
+++ Atividade.02/Atividade.02/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+          // CALCULANDO IDADE //
+Console.WriteLine("Digite o ano em que você nasceu:");
+int anoNascimento = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o ano atual:");
+int anoAtual = int.Parse(Console.ReadLine());
+
+int idade = anoAtual - anoNascimento;
+
+Console.WriteLine("Você tem aproximadamente " + idade + " anos.");
