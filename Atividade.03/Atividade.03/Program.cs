686e43dc2ef62bac01e1533c6f920a152147115b
Fernanda Locatelli <locatelifernanda3@gmail.com>
2025-09-24 08:39:37 -0300

    ATIVIDADE03

---
+++ Atividade.03/Atividade.03/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+                     // CONVERTENDO CELSIUS - FAHRENHEIT //
+Console.WriteLine("Digite a temperatura em Celsius:");
+double celsius = double.Parse(Console.ReadLine());
+
+double fahrenheit = (celsius * 9 / 5) + 32;
+
+Console.WriteLine("A temperatura em Fahrenheit é: " + fahrenheit);
